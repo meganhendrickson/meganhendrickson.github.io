d7623ccfc8753ff4c9b530c24e6568c2d7d04efd
Megan Hendrickson <43715466+meganhendrickson@users.noreply.github.com>
2020-01-17 10:26:34 -0600

    Create ACME

---
+++ ACME/common/footer.php
@@ -0,0 +1,3 @@
+<p>&copy; <?php echo date("Y");?> | ACME | All Rights Reserved</p>
+<p>All images used are believed to be in "Fair Use". Please notify author if any are not and they will be removed.
+<p><?php echo "Last modified" . date(" F d, Y.", getlastmod()); ?></p>
---
+++ ACME/common/header.php
@@ -0,0 +1,17 @@
+<div class="accountmenu">
+    <a href="#"><img src="images/site/account.gif" alt="account icon" id="accounticon"/> My Account</a>
+</div>
+<div class ="logo">
+    <a href="#"><img src="images/site/logo.gif" alt="acme logo" id="logo"/></a>
+</div>
+<nav>
+    <ul class="navigation">
+        <li><a href="#" title="Menu" onclick="toggleMenu()">&#9776; Menu</a></li>
+        <li><a href="#" title="Home">Home</a></li>
+        <li><a href="#" title="Cannon">Cannon</a></li>
+        <li><a href="#" title="Explosive">Explosive</a></li>
+        <li><a href="#" title="Misc">Misc</a></li>
+        <li><a href="#" title="Rocket">Rocket</a></li>
+        <li><a href="#" title="Trap">Trap</a></li>
+    </ul>
+</nav>
