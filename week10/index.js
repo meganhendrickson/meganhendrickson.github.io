4f57e05b215fbe3daabb15778041ec543400eb28
Megan Hendrickson <dearmegs@gmail.com>
2022-05-18 12:57:28 -0700

    Portfolio updates
    removing outdated projects adding new

--- week10/index.js
+++
@@ -1,5 +0,0 @@
-import { getJSON, getLocation } from './utilities.js';
-import QuakesController from './QuakesController.js';
-
-const myQuakesController = new QuakesController('#quakeList');
-myQuakesController.init();
