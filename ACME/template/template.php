d5d1be381234e4ce1e6f576fcea19a1e87b7f907
Megan Hendrickson <dearmegs@gmail.com>
2022-05-18 13:46:21 -0700

    index update

--- ACME/template/template.php
+++
@@ -1,9 +0,0 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/ACME/common/header.php'?>
-
-<!-- ACME TEMPLATE -->  
-<!-- PAGE CONTENT -->
-<section>
-    <h2>content goes here</h2>
-</section>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/ACME/common/footer.php'?>
